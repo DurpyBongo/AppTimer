df6d151bd57529b58964dab88972d39920ebb798
Ethan Zhu <ethanyichenzhu@gmail.com>
2026-01-07 22:32:27 -0500

    Grabbed elements

---
+++ script.js
@@ -0,0 +1,11 @@
+const permissionSection = document.getElementById("permissionSection");
+const requestPermissionBtn = document.getElementById("requestPermissionBtn");
+
+const timerForm = document.getElementById("timerForm");
+const appNameInput = document.getElementById("appName");
+const hoursInput = document.getElementById("hours");
+const minutesInput = document.getElementById("minutes");
+const secondsInput = document.getElementById("seconds");
+const timerSoundInput = document.getElementById("timerSound");
+
+const timersList = document.getElementById("timersList");
